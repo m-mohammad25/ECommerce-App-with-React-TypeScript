30829d759e739ad96e7b203de5795c03f05abfd9
Mohammad Mohammad <m.mohammad25@outlook.com>
2024-10-04 16:40:12 +0300

    combining redux=presist into the project to persist store states

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import { createRoot } from "react-dom/client";
 //redux
 import { Provider } from "react-redux";
-import store from "@store/index";
+import { store, persistor } from "@store/index";
 
 //style
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import AppRouter from "@routes/AppRouter";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <AppRouter />
+    <PersistGate loading={null} persistor={persistor}>
+      {/* "loading={null}: dont show loading screen when fetching data from the storage" */}
+      <AppRouter />
+    </PersistGate>
   </Provider>
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import categories from "@store/categories/categoriesSlice";
 import products from "@store/Products/productsSlice";
 import cart from "@store/Cart/cartSlice";
+
+const persistConfig = {
+  key: "root", //id for this configs
+  storage,
+  whiteList: ["cart"], //presist this state
+};
+
+const rootReducer = combineReducers({ categories, products, cart });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: { categories, products, cart },
+  reducer: persistedReducer,
 });
+
+const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export default store;
+export { store, persistor };
