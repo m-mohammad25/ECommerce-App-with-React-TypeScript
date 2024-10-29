cf4be10e985023c95b866b3827952eecf37aeb07
Mohammad Mohammad <m.mohammad25@outlook.com>
2024-10-29 20:22:18 +0300

    after applying react hook form to registeration form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,39 @@
+import { Heading } from "@components/Common";
+import { Form, Button, Row, Col } from "react-bootstrap";
+
 function Login() {
-  return <div className="login">Login</div>;
+  return (
+    <>
+      <Heading title="User Login" />
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter email"
+                name="email"
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+              />
+            </Form.Group>
+
+            <Button variant="info" type="submit" style={{ color: "white" }}>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default Login;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,78 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Heading } from "@components/Common";
+import { Form, Button, Row, Col } from "react-bootstrap";
+
+type TFormInputs = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 function Register() {
-  return <div className="register">Register</div>;
+  const { register, handleSubmit } = useForm<TFormInputs>();
+  const onSubmit: SubmitHandler<TFormInputs> = (data) => {
+    console.log(data);
+  };
+  return (
+    <>
+      <Heading title="User Registeration" />
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Form className="mb-4" onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className="mb-3">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="First Name"
+                {...register("firstName")}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Last Name"
+                {...register("lastName")}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter email"
+                {...register("email")}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                {...register("password")}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Confirm Password"
+                {...register("confirmPassword")}
+              />
+            </Form.Group>
+
+            <Button variant="info" type="submit" style={{ color: "white" }}>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default Register;
