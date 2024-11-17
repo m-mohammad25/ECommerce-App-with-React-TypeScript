4b88d755b67b034a31ff55e4a5b8f85264662ca5
Mohammad Mohammad <m.mohammad25@outlook.com>
2024-11-17 17:22:20 +0300

    changing the json-server url to the public one

--- src/services/axios_config.js
+++ src/services/axios_config.js
@@ -1,2 +1,4 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:5005/";
+axios.defaults.baseURL =
+  "https://my-json-server.typicode.com/m-mohammad25/ECommerce-App-with-React-TypeScript";
+//"http://localhost:5005/";
