e6854fb66bec1f23f179cefd8e0876c706ee0537
Mohammad Mohammad <m.mohammad25@outlook.com>
2025-01-02 20:15:17 +0300

    `feat: modified actAuthLogin to use ordinary json-server and return fake access token`

--- src/store/Auth/act/actAuthLogin.ts
+++ src/store/Auth/act/actAuthLogin.ts
@@ -19,10 +19,25 @@ type TResponse = {
 const actAuthLogin = createAsyncThunk(
   "auth/actAuthLogin",
   async (formData: TFormDAta, thunk) => {
+    const FAKE_ACCESS_TOKEN =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImgucG90dGVyQGhvZ3dhcnRzLmVkdSIsImlhdCI6MTczNTgyNjQzMCwiZXhwIjoxNzM1ODMwMDMwLCJzdWIiOiIyIn0.QbxEtEteL2cbXna-ykNCvzanu15ZfLu3cLSSwyoQm2A";
     const { rejectWithValue } = thunk;
+
     try {
-      const response = await axios.post<TResponse>("/login", formData);
-      return response.data;
+      const response = await axios.get(
+        `/users?email=${formData.email}&password=${formData.password}`
+      );
+      console.log(response.data);
+      if (response.data.length == 0) return rejectWithValue("Can't find user");
+      return {
+        accessToken: FAKE_ACCESS_TOKEN,
+        user: {
+          id: response.data[0].id,
+          firstName: response.data[0].firstName,
+          lastName: response.data[0].lastName,
+          email: response.data[0].email,
+        },
+      };
     } catch (error) {
       return rejectWithValue(axiosErrorHandler(error));
     }
